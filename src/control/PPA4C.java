2fb12cfd469f3452e9a2a20aaea882e546a600ae
Christopher Chavez <chrischavez@gmx.us>
2015-02-18 14:05:10 -0600

    Moved src to toplevel directory; renamed classes to follow convention; Created control package; moved existing EditorWindow mockup to WindowBuilder class

---
+++ src/control/PPA4C.java
@@ -0,0 +1,12 @@
+package control;
+import editor.EditorWindow;
+public class PPA4C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Current task: prototype practice environment window
+		EditorWindow editorWindow = new EditorWindow();
+		editorWindow.main(null);
+	}
+
+}
