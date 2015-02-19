add2a8ab8777942236ec70c575baefd7e93bd5e7
Christopher Chavez <chrischavez@gmx.us>
2015-02-19 17:17:59 -0600

    Remove ignored files

---
+++ src/editor/FormText.java
@@ -0,0 +1,6 @@
+package editor;
+
+public class FormText extends javax.swing.JLabel{
+	//Ideally, this is an example of how objects in the form can be factored
+	//this.setFont(new java.awt.Font("Lucida Console", 0, 13)); // NOI18N
+}
