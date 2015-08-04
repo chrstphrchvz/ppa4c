7ad68e592850a73d73ef6b6b20d9e08e6dd7c4eb
Christopher Chavez <chrischavez@gmx.us>
2015-08-04 17:36:04 -0500

    Add more detailed instructions on TitlePanel
    Replace single JLabel for instructions with 2 row panel, each row with
    3 columns, and use UIManager.getIcon() to retrieve icons used by JTree
    in TopicBrowserPanel.

--- src/title/TitlePanel.java
+++ src/title/TitlePanel.java
@@ -2,11 +2,13 @@ package title;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 public class TitlePanel extends JPanel {
@@ -14,8 +16,16 @@ public class TitlePanel extends JPanel {
 	private javax.swing.JLabel titleJLabel;
 	private JPanel rightColumnPanel;
 	private JPanel leftColumnPanel;
-	private JLabel footerJLabel;
 	private JPanel twoColumnPanel;
+	private JPanel footerPanel;
+	private JPanel topFooterPanel;
+	private JPanel bottomFooterPanel;
+	private JLabel footerJLabel1;
+	private JLabel footerJLabel2;
+	private JLabel footerJLabel3;
+	private JLabel footerJLabel4;
+	private JLabel footerJLabel5;
+	private JLabel footerJLabel6;
 
 	/**
 	 * Create the panel.
@@ -46,9 +56,37 @@ public class TitlePanel extends JPanel {
 		rightColumnPanel.setLayout(new BorderLayout(0, 0));
 		rightColumnPanel.add(titleJLabel);
 		
-		footerJLabel = new JLabel("<html><h1>Click on a topic to begin</h1></html>");
-		footerJLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		add(footerJLabel, BorderLayout.SOUTH);
+		footerPanel = new JPanel();
+		footerPanel.setLayout(new BoxLayout(footerPanel,BoxLayout.Y_AXIS));
+		footerPanel.setOpaque(false);
+		
+		topFooterPanel = new JPanel();
+		topFooterPanel.setLayout(new BoxLayout(topFooterPanel,BoxLayout.X_AXIS));
+		topFooterPanel.setOpaque(false);
+		footerPanel.add(topFooterPanel);
+		
+		bottomFooterPanel = new JPanel();
+		bottomFooterPanel.setLayout(new BoxLayout(bottomFooterPanel,BoxLayout.X_AXIS));
+		bottomFooterPanel.setOpaque(false);
+		footerPanel.add(bottomFooterPanel);
+		
+		footerJLabel1 = new JLabel("<html><h2>Click on a&nbsp;</h2></html>");
+		footerJLabel1.setHorizontalAlignment(SwingConstants.RIGHT);
+		footerJLabel2 = new JLabel(UIManager.getIcon("Tree.leafIcon"));
+		footerJLabel3 = new JLabel("<html><h2>&nbsp;to begin a topic</h2></html>");
+		topFooterPanel.add(footerJLabel1);
+		topFooterPanel.add(footerJLabel2);
+		topFooterPanel.add(footerJLabel3);
+		
+		footerJLabel4 = new JLabel("<html><h2>Expand a&nbsp;</h2></html>");
+		footerJLabel4.setHorizontalAlignment(SwingConstants.RIGHT);
+		footerJLabel5 = new JLabel(UIManager.getIcon("Tree.closedIcon"));
+		footerJLabel6 = new JLabel("<html><h2>&nbsp;to show more topics</h2></html>");
+		bottomFooterPanel.add(footerJLabel4);
+		bottomFooterPanel.add(footerJLabel5);
+		bottomFooterPanel.add(footerJLabel6);
+		
+		add(footerPanel, BorderLayout.SOUTH);
 
 	}
 
